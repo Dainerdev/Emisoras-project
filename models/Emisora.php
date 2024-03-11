2290f7e666dea351182f9819292bb18310883175
Dainerdev <florianarroyo15@gmail.com>
2024-03-11 18:12:11 -0500

    Actualizacion de archivos

--- models/Emisora.php
+++ models/Emisora.php
@@ -3,4 +3,8 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "emisoras/libs/config.php";
 
 class Emisora extends ActiveRecord\Model{
     
+    public static $primary_key = "nombre";
+    public static $belongs_to = array(array("Transmision"));    #le pertenece a (uno a uno)
+
+
 }
--- models/Transmision.php
+++ models/Transmision.php
@@ -3,4 +3,9 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "emisoras/libs/config.php";
 
 class Transmision extends ActiveRecord\Model{
     
+    public static $primary_key = "nombre";
+    public static $has_many = array(array("Emisoras"), array("Frecuencia"));    #le pertenece a (uno a uno)
+
+
+
 }
